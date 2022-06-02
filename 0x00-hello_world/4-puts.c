1c2bdf1bfc57bb0e3182d974a854339915ca277a
Elhameed <teniolaajani2004@gmail.com>
2022-06-02 15:11:09 -0700

    added a few things

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  *
  *  * main - Entry point
@@ -10,8 +12,14 @@
  *
  *     */
 
+
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
