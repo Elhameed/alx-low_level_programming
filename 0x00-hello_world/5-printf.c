c6c7078f9c526c6226b35e220b385e3bc8c9a630
Elhameed <teniolaajani2004@gmail.com>
2022-06-08 14:37:08 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,", "%s,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
