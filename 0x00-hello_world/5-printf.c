95c1164c7b8d03121004a7d5d176b92a79eac499
Elhameed <teniolaajani2004@gmail.com>
2022-06-20 06:39:29 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Print a string
- * Description: use printf
+ * Description: use printf to print `with proper grammar, but the outcome is a piece of art,`
  * Return: 0
  */
 
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
