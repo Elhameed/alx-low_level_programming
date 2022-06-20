640d4c1a830cbb2c138e6dba203b2e78e651808d
Elhameed <teniolaajani2004@gmail.com>
2022-06-20 14:34:09 -0700

    rectified 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  * main -Entry point
- * Description: prints allsingle digit numbers of
- * base 10 from 0
+ * Description: prints all single digit numbers from 0
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int i = 0;
