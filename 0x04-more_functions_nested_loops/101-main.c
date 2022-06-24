e562a4b5975b99813cfe29eddfbc665e794c19da
Elhameed <teniolaajani2004@gmail.com>
2022-06-24 16:03:56 -0700

    done already

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	print_number(98);
-        _putchar('\n');
+	_putchar('\n');
 	print_number(402);
         _putchar('\n');
 	print_number(1024);
-        _putchar('\n');
+      	_putchar('\n');
 	print_number(0);
         _putchar('\n');
 	print_number(-98);
