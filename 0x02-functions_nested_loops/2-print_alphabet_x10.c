5778ed957e415a633c8e0a8a9848e98750ef13b9
Abdulhameed Teniola Ajani <teniolaajani2004@gmail.com>
2022-06-28 20:15:57 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	int i;
 	char c;
 
-	while (i < 10)
+	for ( i = 0; i <= 9; i++)
 	{
-		c = 'a';
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		_putchar('\n');
-		i++;
-	}
+        for ( c = 'a';  c <= 'z'; c++)
+        {
+            putchar(c);
+        }
+	putchar('\n');   
+    }
 }
