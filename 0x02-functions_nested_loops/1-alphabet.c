c38ea736346553a2c58a0c0790c16fb2a76a6a12
Abdulhameed Teniola Ajani <teniolaajani2004@gmail.com>
2022-06-28 19:56:17 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
  * print_alphabet - Print alphabet in lowercase using only `_putchar`
  * Return: 0
  */
+
 void print_alphabet(void)
 {
-	char c = 'a';
-
-	while (c <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
-		c++;
 	}
 	_putchar('\n');
 }
