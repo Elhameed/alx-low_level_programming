4b75696eda10831a4ac2d413a7e5495d05a9740b
Elhameed <teniolaajani2004@gmail.com>
2022-06-28 12:31:47 -0700

    modified

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,24 +9,13 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char c;
-<<<<<<< HEAD
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (c = 'a';  c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	putchar('\n');
-=======
-	
-	for ( i = 0; i <= 9; i++)
-	{
-		for ( c = 'a';  c <= 'z'; c++)
+		for (c = 'a';  c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		_putchar('\n');   
->>>>>>> e01aac99af795f668671c66ff67aeb6231e7f72c
+		_putchar('\n');
 	}
 }
