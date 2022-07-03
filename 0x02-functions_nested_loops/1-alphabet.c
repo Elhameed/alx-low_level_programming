16ca923350157f3f812e8a5dbd055fba13698eea
Elhameed <teniolaajani2004@gmail.com>
2022-07-03 05:40:46 -0700

    update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,6 @@
  * Return: 0
  */
 
-void print_alphabet(void);
-
-int main(void) {
-    print_alphabet();
-    return 0;
-}
-
 void print_alphabet(void) {
     int i;
     for ( i = 'a'; i <= 'z'; i++)
