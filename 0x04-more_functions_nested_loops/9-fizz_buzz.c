3098a90603e0373df134149347ae4e5cbc6f1325
Abdulhameed Teniola Ajani <teniolaajani2004@gmail.com>
2022-07-05 13:48:43 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3) == 0)
 		printf("Fizz ");
-		else if (i == 100)
-		printf("Buzz");
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 		printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if ((i % 3) == 0 && i % 5 == 0)
 		printf("FizzBuzz ");
 		else
 		printf("%d ", i);
+		if (i != 100)
+		printf(' ');
 	}
 	printf("\n");
 	return (0);
