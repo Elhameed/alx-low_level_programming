97e3b83acca99fcacfefacf722482382a3c21c58
Elhameed <teniolaajani2004@gmail.com>
2022-07-20 07:04:35 -0700

    made a change

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,17 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
+	printf("%d\n", argc);
 
 	return (0);
 }
