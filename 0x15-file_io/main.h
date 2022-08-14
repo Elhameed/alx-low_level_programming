ed48a7f3e261735f003ec6581f19d026bdd66a46
Abdulhameed Teniola Ajani <teniolaajani2004@gmail.com>
2022-08-14 20:53:10 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
+#endif /* MAIN_H */
